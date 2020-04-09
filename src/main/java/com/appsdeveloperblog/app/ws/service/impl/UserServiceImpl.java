d82a7ca6f9913b3f1ab1f822e974cf2454378cfa
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2020-04-10 01:58:00 +0200

    create user endpoint

---
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.appsdeveloperblog.app.ws.service.impl;
+
+import com.appsdeveloperblog.app.ws.UserRepository;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.Utils;
+import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    Utils utils;
+
+    @Override
+    public UserDto createUser(UserDto user) {
+        if (userRepository.findUserByEmail(user.getEmail()) != null) {
+            throw new RuntimeException("Record already exists");
+        }
+
+        UserEntity userEntity = new UserEntity();
+        BeanUtils.copyProperties(user, userEntity);
+
+        userEntity.setEncryptedPassword("hardcode for now");
+        userEntity.setUserId(utils.generateUserId(30));
+        UserEntity storedUserDetails = userRepository.save(userEntity);
+
+        UserDto returnValue = new UserDto();
+        BeanUtils.copyProperties(storedUserDetails, returnValue);
+
+        return returnValue;
+    }
+
+}
