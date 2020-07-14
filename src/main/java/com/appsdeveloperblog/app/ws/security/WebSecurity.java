2c0eb48c49f87e15456deb43fc654f94746ab283
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2020-07-14 17:29:30 +0100

    user authentication/authorization

--- src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -29,7 +29,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                     .permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .addFilter( getAuthenticationFilter() );
+                .addFilter( getAuthenticationFilter() )
+                .addFilter( new AuthorizationFilter(authenticationManager()) );
     }
 
     @Override
