08d8988edaa4021044ee1ae1eac9163eae88f0a8
Dawid Stodołkiewicz <dawid.stodolkiewicz@empirica.pl>
2020-07-18 00:27:51 +0100

    changes for war deployment on not built-in tomcat

--- src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsApplication.java
@@ -2,11 +2,21 @@ package com.appsdeveloperblog.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class MobileAppWsApplication {
+public class MobileAppWsApplication extends SpringBootServletInitializer {
+
+	/*
+		for building war files
+	*/
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(MobileAppWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
